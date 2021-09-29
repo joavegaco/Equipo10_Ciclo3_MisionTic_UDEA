03c8082fee3f16dae18a1534c0a1de2fda30abac
joavegaco <joavegaco311@gmail.com>
2021-09-29 16:07:27 -0500

    Archivos nuevos en React

---
+++ maqueta-auth/src/auth/LoginPage.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import NavbarComponent from "../shared/components/header/NavbarComponent";
+
+function LoginPage() {
+
+    return (
+    <Fragment>
+        <NavbarComponent /> <h1>Hola Mundo</h1>
+    </Fragment>);
+}
+
+export default LoginPage;
---
+++ maqueta-auth/src/register/RegisterPage.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import NavbarComponent from "../shared/components/header/NavbarComponent";
+
+function RegisterPage() {
+
+    return (<Fragment>
+        <NavbarComponent />
+        <h1>Hola Johan</h1>
+    </Fragment>)
+}
+
+export default RegisterPage;
