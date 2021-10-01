0efa862e2c3029cc02433918382939146fe63e78
joavegaco <joavegaco311@gmail.com>
2021-10-01 08:16:02 -0500

    Cambios 1-10-21

--- maqueta-auth/src/auth/LoginPage.jsx
+++ maqueta-auth/src/auth/LoginPage.jsx
@@ -1,54 +1,56 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import './AtomiumLogin.css'
 
 function LoginPage() {
 
+  const [email,setEmail]= useState("");
+  const [password,setPassword]= useState("");
     return (
     <Fragment>
       <h1>Hola Johan</h1>
         <div className="card-body text-white bg-secondary">
-  <div className="mt-5">
-    <div className="container-fluid">  
-      <div className="row row-cols-3">
-        <div className="col-6 col-xl-4">
-          <p style={{width:"100%"}}>Atomium es una empresa...</p>
-        </div>
-        
-        <div className="col-12 col-xl-4" style={{margin:"auto"}}>
-          
+          <div className="mt-5">
+            <div className="container-fluid">  
+              <div className="row row-cols-3">
+                <div className="col-6 col-xl-4">
+                  <p style={{width:"100%"}}>Atomium es una empresa...</p>
+                </div>
+                
+                <div className="col-12 col-xl-4" style={{margin:"auto"}}>
+                  
 
-          <div className="login">
-          
-            <h2 className="login-header">Bienvenido</h2>
-          
-            <form className="login-container">
-              <h6>EMAIL</h6>
-              <p><input type="texto" placeholder="Email"/></p>
-              <h6>CONTRASEÑA</h6>
-              <p><input type="password" placeholder="Password"/></p>
-              <p> <input type="submit" value="Entrar"/> </p>
-            </form>
-            <hr style={{backgroundColor:"rgb(255, 255, 255)",margin:"auto"}}/>
-            
+                  <div className="login">
+                  
+                    <h2 className="login-header">Bienvenido</h2>
+                  
+                    <form className="login-container">
+                      <h6>EMAIL</h6>
+                      <p><input type="email" placeholder="Email" onChange={(data) => setEmail(data.target.value)}/></p>
+                      <h6>CONTRASEÑA</h6>
+                      <p><input type="password" placeholder="Password" onChange={(data) => setPassword(data.target.value)}/></p>
+                      <p> <input type="submit" value="Entrar"/> </p>
+                    </form>
+                    <hr style={{backgroundColor:"rgb(255, 255, 255)",margin:"auto"}}/>
+                    
 
-            
-            <div className="mt-3 mb-3">
-              <h6 style={{margin:"auto"}}>Acceda a sus funciones como vendedor, administrador, ejecutivo, operario, director o gerente comercial</h6>
-              <a style={{margin:"auto", color:"rgb(0, 137, 250)"}} href='https://www.w3schools.com/'>Regístrese aquí</a><br/>
-              <a style={{margin:"auto", color:"rgb(0, 137, 250)"}} href='https://www.w3schools.com/'>Olvidó su contraseña</a>
-              <div className="mt-3"><hr style={{backgroundColor: "rgb(255, 255, 255)", margin:"auto"}} /></div>
+                    
+                    <div className="mt-3 mb-3">
+                      <h6 style={{margin:"auto"}}>Acceda a sus funciones como vendedor, administrador, ejecutivo, operario, director o gerente comercial</h6>
+                      <a style={{margin:"auto", color:"rgb(0, 137, 250)"}} href='https://www.w3schools.com/'>Regístrese aquí</a><br/>
+                      <a style={{margin:"auto", color:"rgb(0, 137, 250)"}} href='https://www.w3schools.com/'>Olvidó su contraseña</a>
+                      <div className="mt-3"><hr style={{backgroundColor: "rgb(255, 255, 255)", margin:"auto"}} /></div>
+                    </div>
+                  </div>
+                  
+                </div>
+                
+                
+                <div className="col-12 col-6 col-xl-4" style={{backgroundImage:" url('assets/img/ImagologoAtomiumTransparente-01.png')",backgroundRepeat: "no-repeat"}}></div>
+              
+              </div>
             </div>
           </div>
-          
-        </div>
-        
-        
-        <div className="col-12 col-6 col-xl-4" style={{backgroundImage:" url('assets/img/ImagologoAtomiumTransparente-01.png')",backgroundRepeat: "no-repeat"}}></div>
-       
-      </div>
-    </div>
-  </div>
-  </div>
+          </div>
     </Fragment>);
 }
 
