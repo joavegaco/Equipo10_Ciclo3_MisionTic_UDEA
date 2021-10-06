d2b969f53de3995800f087ad7c099de179b68635
joavegaco <joavegaco311@gmail.com>
2021-10-06 08:01:32 -0500

    Borrado firebase

--- maqueta-backend/app.js
+++ maqueta-backend/app.js
@@ -7,8 +7,29 @@ const port = 5000
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/get-product', (req, res) => {
+  res.send('Hola Mundo')
+})
+
+app.post('/add-product',(req,res) =>{
+  const product = req.body;
+  console.log(product.name);
+  res.json('Hola Mundo')
+  
+})
+
+app.put('/update-product',(req,res) =>{
+  const product = req.body;
+  console.log(product.name);
+  res.json('Hola Mundo')
+  
+})
+
+app.delete('/delete-product',(req,res) =>{
+  const product = req.body;
+  console.log(product.name);
+  res.json('Hola Mundo')
+  
 })
 
 app.listen(port, () => {
